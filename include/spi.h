1688e279ba162900509e6b538dc69c7ee480eb5c
Lukas Jäger <lukas_jaeger@gmx.net>
2017-11-21 18:23:25 +0100

    Added SPI-code

---
+++ include/spi.h
@@ -0,0 +1,31 @@
+/**\file */
+#ifndef _SPI_H_
+#define _SPI_H_
+
+/* 
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. 
+ */
+
+#include "lazy.h"
+
+/**
+ * Sets up the USI-interface B as an SPI-interface.
+ * @param cs_line_port The port of the CS-line
+ * @param cs_line_pin The CS-line
+ * @return _OK_ if the setup was successful and _ERR_ otherwise
+ */
+RC spi_setup(size_t cs_line_port, size_t cs_line_pin);
+
+/**
+ * Transmits a single char over the USI-interface B. Take care
+ * of the CS-line yourself, please.
+ * @param data The char to transmit
+ * @return The received char
+ */
+uint8_t spi_transmit(uint8_t data);
+
+#endif //_SPI_H_
---
+++ src/spi.c
@@ -0,0 +1,43 @@
+#include "spi.h"
+
+RC spi_setup(size_t cs_line_port, size_t cs_line_pin){
+	switch (cs_line_port){
+		case 1:
+			if (cs_line_pin > 4){
+				return ERR;
+			}
+			P1DIR |= BM(cs_line_pin);
+			P1OUT |= BM(cs_line_pin);
+			break;
+		case 2:
+			if (cs_line_pin > 5){
+				return ERR;
+			}
+			P2DIR |= BM(cs_line_pin);
+			P2OUT |= BM(cs_line_pin);
+			break;
+		default:
+			return ERR;
+	}
+	// Configuring pins for USI B
+	P1SEL  |= BIT5 | BIT6 | BIT7;
+	P1SEL2 |= BIT5 | BIT6 | BIT7;
+
+	// Configuring USI B
+	UCB0CTL1 = UCSWRST;
+	UCB0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC;
+	UCB0CTL1 |= UCSSEL_2;	
+	UCB0BR0 = 0x02;
+	UCB0BR1 = 0;
+	UCA0MCTL = 0;
+	UCA0CTL1 &= ~UCSWRST;
+
+	return OK;
+}
+
+uint8_t spi_transmit(uint8_t data){
+	while(!(IFG2 & UCB0TXIFG));
+	UCB0TXBUF = data;
+	while(!(IFG2 & UCB0RXIFG));
+	return UCB0RXBUF;
+}
